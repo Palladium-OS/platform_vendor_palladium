565ddf3e33338dcc0669b3459708955f7e5b4ef5
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-22 16:13:10 -0300

    vendor: build: Generate json file with zip info after build
    Change-Id: I98ba732b4a239e38476c8a55822583fdefa68008
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- build/tasks/palladium.mk
+++ build/tasks/palladium.mk
@@ -37,6 +37,7 @@ otapackage: $(INTERNAL_OTA_PACKAGE_TARGET)
 palladium: otapackage
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(PALLADIUM_TARGET_PACKAGE)
 	$(hide) $(MD5) $(PALLADIUM_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(PALLADIUM_TARGET_PACKAGE).md5sum
+	$(hide) ./vendor/palladium/tools/generate_json_build_info.sh $(PALLADIUM_TARGET_PACKAGE)
 	@echo -e ""
 	@echo -e "${cya}Building ${bldcya}Palladium ! ${txtrst}";
 	@echo -e ""
