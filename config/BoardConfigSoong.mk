e75715a15c9646abe1b2832b40cf29480cb20064
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-22 12:36:48 -0500

    soong: Add TARGET_LEGACY_HW_DISK_ENCRYPTION conditional
    Change-Id: I2c67fe632d415353273839493a86cdd75bd93b23

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -49,6 +49,7 @@ SOONG_CONFIG_palladiumNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += palladiumQcomVars
 SOONG_CONFIG_palladiumQcomVars += \
+    legacy_hw_disk_encryption \
     should_wait_for_qsee \
     supports_extended_compress_format \
     supports_hw_fde \
@@ -73,6 +74,7 @@ SOONG_CONFIG_palladiumGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEED
 SOONG_CONFIG_palladiumGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_palladiumGlobalVars_target_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_palladiumNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_palladiumQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_palladiumQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_palladiumQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_palladiumQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
