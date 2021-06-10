aeaae2318394e2b55630b8e663442be843725d66
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-11 00:39:41 +0200

    soong: Add TARGET_CAMERA_BOOTTIME_TIMESTAMP conditional
    Change-Id: Ic48acda208fc9990fc5c9159ad06518d48f21477

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -36,6 +36,7 @@ SOONG_CONFIG_palladiumGlobalVars += \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
+    needs_camera_boottime \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -70,6 +71,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_palladiumGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_palladiumGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_palladiumGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_palladiumGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_palladiumGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_palladiumNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
