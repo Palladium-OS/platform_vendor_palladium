Project-Xtended/vendor_xtended

envsetup: Build signed zip

build/envsetup.sh
@@ -613,7 +613,13 @@ function xtendedrebase() {
 }
 
 function mka() {
-    m -j "$@"
+    if [ "$1" = "signed-zip" ]; then
+        m -j target-files-package otatools
+        croot
+        source vendor/xtended/scripts/sign.sh
+    else
+        m -j "$@"
+    fi
 }
 
 function cmka() {

Palladium-OS/platform_vendor_palladium

build/envsetup.sh
@@ -613,7 +613,13 @@ function palladiumrebase() {
 }
 
 function mka() {
-    m -j "$@"
+    if [ "$1" = "signed-zip" ]; then
+        m -j target-files-package otatools
+        croot
+        source vendor/palladium/scripts/sign.sh
+    else
+        m -j "$@"
+    fi
 }
 
 function cmka() {