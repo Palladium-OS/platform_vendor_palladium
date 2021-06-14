Project-Xtended/vendor_xtended

vendor: Handle errors gracefully
Signed-off-by: Dev <devvmashru@protonmail.ch>

build/envsetup.sh
@@ -614,9 +614,10 @@ function xtendedrebase() {
 
 function mka() {
     if [ "$1" = "signed-zip" ]; then
-        m -j target-files-package otatools
-        croot
-        source vendor/xtended/scripts/sign.sh
+        if m -j target-files-package otatools; then
+            croot
+            source vendor/xtended/scripts/sign.sh
+        fi
     else
         m -j "$@"
     fi

Palladium-OS/platform_vendor_palladium

build/envsetup.sh
@@ -614,9 +614,10 @@ function palladiumrebase() {
 
 function mka() {
     if [ "$1" = "signed-zip" ]; then
-        m -j target-files-package otatools
-        croot
-        source vendor/palladium/scripts/sign.sh
+        if m -j target-files-package otatools; then
+            croot
+            source vendor/palladium/scripts/sign.sh
+        fi
     else
         m -j "$@"
     fi