Project-Xtended/vendor_xtended

envsetup: Generate OTA JSON only on successful completion of build
Signed-off-by: Dev <devvmashru@protonmail.ch>

build/envsetup.sh
@@ -616,8 +616,9 @@ function mka() {
     if [ "$1" = "signed-zip" ]; then
         if m -j target-files-package otatools; then
             croot
-            source vendor/xtended/scripts/sign.sh
-            source vendor/xtended/scripts/update.sh
+            if source vendor/xtended/scripts/sign.sh; then
+                source vendor/xtended/scripts/update.sh
+            fi
         fi
     else
         m -j "$@"

Palladium-OS/platform_vendor_palladium

build/envsetup.sh
@@ -616,8 +616,9 @@ function mka() {
     if [ "$1" = "signed-zip" ]; then
         if m -j target-files-package otatools; then
             croot
-            source vendor/palladium/scripts/sign.sh
-            source vendor/palladium/scripts/update.sh
+            if source vendor/palladium/scripts/sign.sh; then
+                source vendor/palladium/scripts/update.sh
+            fi
         fi
     else
         m -j "$@"