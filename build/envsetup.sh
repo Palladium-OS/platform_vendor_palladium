Project-Xtended/vendor_xtended

vendor: Generate OTA JSON for all official builds
Signed-off-by: Dev <devvmashru@protonmail.ch>

build/envsetup.sh
@@ -621,7 +621,10 @@ function mka() {
             fi
         fi
     else
-        m -j "$@"
+        if m -j "$@"; then
+            croot
+            source vendor/xtended/scripts/update.sh
+        fi
     fi
 }
 

scripts/update.sh
@@ -3,9 +3,14 @@
 zip=$(get_build_var XTENDED_VERSION).zip
 device=$(echo $TARGET_PRODUCT | cut -d '_' -f2)
 buildprop=$OUT/system/build.prop
-romtype="OFFICIAL"
+linen=$(grep -n "ro.xtended.build.type" $buildprop | cut -d ':' -f1)
+romtype=$(sed -n $linen'p' < $buildprop | cut -d '=' -f2)
 variant=$(echo $zip | cut -d '-' -f4)
 name=$device'_'$variant.json
+
+if [ "$romtype" != "OFFICIAL" ]; then
+	return 0
+fi
 
 if [ -f $OUT/$name ]; then
 	rm $OUT/$name

Palladium-OS/platform_vendor_palladium

build/envsetup.sh
@@ -621,7 +621,10 @@ function mka() {
             fi
         fi
     else
-        m -j "$@"
+        if m -j "$@"; then
+            croot
+            source vendor/palladium/scripts/update.sh
+        fi
     fi
 }
 

scripts/update.sh
@@ -3,9 +3,14 @@
 zip=$(get_build_var PALLADIUM_VERSION).zip
 device=$(echo $TARGET_PRODUCT | cut -d '_' -f2)
 buildprop=$OUT/system/build.prop
-romtype="OFFICIAL"
+linen=$(grep -n "ro.palladium.build.type" $buildprop | cut -d ':' -f1)
+romtype=$(sed -n $linen'p' < $buildprop | cut -d '=' -f2)
 variant=$(echo $zip | cut -d '-' -f4)
 name=$device'_'$variant.json
+
+if [ "$romtype" != "OFFICIAL" ]; then
+	return 0
+fi
 
 if [ -f $OUT/$name ]; then
 	rm $OUT/$name