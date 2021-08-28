ankan005/vendor_xtended-1

vendor: Generate OTA JSON
Signed-off-by: Dev <devvmashru@protonmail.ch>

build/envsetup.sh
@@ -617,6 +617,7 @@ function mka() {
         if m -j target-files-package otatools; then
             croot
             source vendor/xtended/scripts/sign.sh
+            source vendor/xtended/scripts/update.sh
         fi
     else
         m -j "$@"

scripts/update.sh
@@ -1,0 +1,29 @@
+#!/bin/bash
+
+zip=$(get_build_var XTENDED_VERSION).zip
+device=$(echo $TARGET_PRODUCT | cut -d '_' -f2)
+buildprop=$OUT/system/build.prop
+romtype="OFFICIAL"
+
+if [ -f $OUT/$device.json ]; then
+	rm $OUT/$device.json
+fi
+
+linenr=$(grep -n "ro.system.build.date.utc" $buildprop | cut -d ':' -f1)
+datetime=$(sed -n $linenr'p' < $buildprop | cut -d '=' -f2)
+id=$(md5sum "$OUT/$zip" | cut -d ' ' -f1)
+size=$(stat -c "%s" "$OUT/$zip")
+version=$(echo "$zip" | cut -d '-' -f3)
+echo '{
+	"response": [
+		{
+			"datetime": '$datetime',
+			"filename": "'$zip'",
+			"id": "'$id'",
+			"romtype": "'$romtype'",
+			"size": '$size',
+			"url": "https:\/\/downloads.msmxtended.org\/'$device'\/'$zip'",
+			"version": "'$version'"
+		}
+	]
+}' >> $device.json

Palladium-OS/platform_vendor_palladium

build/envsetup.sh
@@ -617,6 +617,7 @@ function mka() {
         if m -j target-files-package otatools; then
             croot
             source vendor/palladium/scripts/sign.sh
+            source vendor/palladium/scripts/update.sh
         fi
     else
         m -j "$@"

scripts/update.sh
@@ -1,0 +1,29 @@
+#!/bin/bash
+
+zip=$(get_build_var PALLADIUM_VERSION).zip
+device=$(echo $TARGET_PRODUCT | cut -d '_' -f2)
+buildprop=$OUT/system/build.prop
+romtype="OFFICIAL"
+
+if [ -f $OUT/$device.json ]; then
+	rm $OUT/$device.json
+fi
+
+linenr=$(grep -n "ro.system.build.date.utc" $buildprop | cut -d ':' -f1)
+datetime=$(sed -n $linenr'p' < $buildprop | cut -d '=' -f2)
+id=$(md5sum "$OUT/$zip" | cut -d ' ' -f1)
+size=$(stat -c "%s" "$OUT/$zip")
+version=$(echo "$zip" | cut -d '-' -f3)
+echo '{
+	"response": [
+		{
+			"datetime": '$datetime',
+			"filename": "'$zip'",
+			"id": "'$id'",
+			"romtype": "'$romtype'",
+			"size": '$size',
+			"url": "https:\/\/downloads.palladiumos.com\/'$device'\/'$zip'",
+			"version": "'$version'"
+		}
+	]
+}' >> $device.json