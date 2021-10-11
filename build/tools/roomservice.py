Project-Fluid/vendor_fluid

roomservice: allow replacing already existing projects using deps

build/tools/roomservice.py
@@ -48,6 +48,10 @@ custom_dependencies = "fluid.dependencies"
 org_manifest = "fluid-devices"  # leave empty if org is provided in manifest
 org_display = "Project-Fluid-Devices"  # needed for displaying
 
+default_manifest = ".repo/manifests/default.xml"
+fluid_manifest = ".repo/manifests/snippets/fluid.xml"
+lineage_manifest = ".repo/manifests/snippets/lineage.xml"
+
 github_auth = os.getenv('GITHUB_API_TOKEN', None)
 
 
@@ -77,6 +81,14 @@ def add_auth(g_req):
     if github_auth:
         g_req.add_header("Authorization", "token %s" % github_auth)
 
+def exists_in_tree(lm, repository):
+     for child in lm.getchildren():
+        try:
+            if child.attrib['path'].endswith(repository):
+                return child
+        except:
+            pass
+     return None
 
 def indent(elem, level=0):
     # in-place prettyprint formatter
@@ -121,6 +133,9 @@ def is_in_manifest(project_path):
 
 def add_to_manifest(repos, fallback_branch=None):
     lm = load_manifest(custom_local_manifest)
+    mlm = load_manifest(default_manifest)
+    fluidm = load_manifest(fluid_manifest)
+    lineagem = load_manifest(lineage_manifest)
 
     for repo in repos:
         repo_name = repo['repository']
@@ -139,6 +154,21 @@ def add_to_manifest(repos, fallback_branch=None):
         if is_in_manifest(repo_path):
             print('already exists: %s' % repo_path)
             continue
+
+        existing_m_project = None
+        if exists_in_tree(mlm, repo_target) != None:
+           existing_m_project = exists_in_tree(mlm, repo_target)
+        elif exists_in_tree(fluidm, repo_target) != None:
+             existing_m_project = exists_in_tree(fluidm, repo_target)
+        elif exists_in_tree(lineagem, repo_target) != None:
+             existing_m_project = exists_in_tree(lineagem, repo_target)
+
+        if existing_m_project != None:
+            if existing_m_project.attrib['path'] == repo['target_path']:
+                print('%s already exists in main manifest, replacing with new dep' % repo_name)
+                lm.append(ElementTree.Element("remove-project", attrib = {
+                    "name": existing_m_project.attrib['name']
+                }))
 
         print('Adding dependency:\nRepository: %s\nBranch: %s\nRemote: %s\nPath: %s\n' % (repo_name, repo_branch,repo_remote, repo_path))
 

Palladium-OS/platform_vendor_palladium

build/tools/roomservice.py
@@ -48,6 +48,10 @@ custom_dependencies = "palladium.dependencies"
 org_manifest = "palladium-devices"  # leave empty if org is provided in manifest
 org_display = "PalladiumOS-Devices"  # needed for displaying
 
+default_manifest = ".repo/manifests/default.xml"
+palladium_manifest = ".repo/manifests/snippets/palladium.xml"
+lineage_manifest = ".repo/manifests/snippets/lineage.xml"
+
 github_auth = os.getenv('GITHUB_API_TOKEN', None)
 
 
@@ -77,6 +81,14 @@ def add_auth(g_req):
     if github_auth:
         g_req.add_header("Authorization", "token %s" % github_auth)
 
+def exists_in_tree(lm, repository):
+     for child in lm.getchildren():
+        try:
+            if child.attrib['path'].endswith(repository):
+                return child
+        except:
+            pass
+     return None
 
 def indent(elem, level=0):
     # in-place prettyprint formatter
@@ -121,6 +133,9 @@ def is_in_manifest(project_path):
 
 def add_to_manifest(repos, fallback_branch=None):
     lm = load_manifest(custom_local_manifest)
+    mlm = load_manifest(default_manifest)
+    palladiumm = load_manifest(palladium_manifest)
+    lineagem = load_manifest(lineage_manifest)
 
     for repo in repos:
         repo_name = repo['repository']
@@ -139,6 +154,21 @@ def add_to_manifest(repos, fallback_branch=None):
         if is_in_manifest(repo_path):
             print('already exists: %s' % repo_path)
             continue
+
+        existing_m_project = None
+        if exists_in_tree(mlm, repo_target) != None:
+           existing_m_project = exists_in_tree(mlm, repo_target)
+        elif exists_in_tree(palladiumm, repo_target) != None:
+             existing_m_project = exists_in_tree(palladiumm, repo_target)
+        elif exists_in_tree(lineagem, repo_target) != None:
+             existing_m_project = exists_in_tree(lineagem, repo_target)
+
+        if existing_m_project != None:
+            if existing_m_project.attrib['path'] == repo['target_path']:
+                print('%s already exists in main manifest, replacing with new dep' % repo_name)
+                lm.append(ElementTree.Element("remove-project", attrib = {
+                    "name": existing_m_project.attrib['name']
+                }))
 
         print('Adding dependency:\nRepository: %s\nBranch: %s\nRemote: %s\nPath: %s\n' % (repo_name, repo_branch,repo_remote, repo_path))
 