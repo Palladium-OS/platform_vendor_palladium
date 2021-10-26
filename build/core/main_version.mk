92e6e67038ec2a01d8de3173cc3b1afad257abc8
zoldycc <sanjheevisv@palladiumos.com>
2021-10-26 18:21:24 +0530

    vendor: build few more props
    Change-Id: Ibd3b987519f7fcdc78844dbf0dd25b3ff19d9d8e

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -8,8 +8,10 @@ endif
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.palladium.display.version=$(PALLADIUM_DISPLAY_VERSION) \
     ro.palladium.build.version=$(PALLADIUM_BUILD_VERSION) \
+    ro.palladium.num.version=$(PALLADIUM_NUM_VER) \
     ro.palladium.build.date=$(BUILD_DATE) \
     ro.palladium.buildtype=$(PALLADIUM_BUILD_TYPE) \
+    ro.palladium.build.status=$(PALLADIUM_BUILD_TYPE) \
     ro.palladium.fingerprint=$(ROM_FINGERPRINT) \
     ro.palladium.version=$(PALLADIUM_VERSION) \
     ro.modversion=$(PALLADIUM_VERSION)
