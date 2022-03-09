Project-Awaken/android_vendor_awaken

vendor: Add SettingsIntelligenceGooglePrebuilt
* From redfin-user-12-SP1A.210812.015
* Also update gapps path

Change-Id: I75769db0b6c590dfdca550ee2ec491ca2438a5b9
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

config/common.mk
@@ -77,7 +77,7 @@ include vendor/awaken/config/fonts.mk
 
 # Gapps
 ifeq ($(USE_GAPPS),true)
-$(call inherit-product, vendor/gapps/gapps.mk)
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 endif
 
 
@@ -106,7 +106,8 @@ $(call inherit-product, vendor/awaken/config/bootanimation.mk)
 
 # Config
 PRODUCT_PACKAGES += \
-    SimpleDeviceConfig
+    SimpleDeviceConfig \
+    SettingsIntelligenceGooglePrebuilt
 
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -115,6 +116,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
     procmem
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/awaken/config/permissions/privapp-permissions-awaken-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-awaken.xml
 
 # Root
 PRODUCT_PACKAGES += \

Palladium-OS/platform_vendor_palladium

config/common.mk
@@ -81,7 +81,7 @@ include vendor/palladium/config/fonts.mk
 
 # Gapps
 ifeq ($(USE_GAPPS),true)
-$(call inherit-product, vendor/gapps/gapps.mk)
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 endif
 
 
@@ -110,7 +110,8 @@ $(call inherit-product, vendor/palladium/bootanimation/bootanimation.mk)
 
 # Config
 PRODUCT_PACKAGES += \
-    SimpleDeviceConfig
+    SimpleDeviceConfig \
+    SettingsIntelligenceGooglePrebuilt
 
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -119,6 +120,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
     procmem
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/palladium/config/permissions/privapp-permissions-palladium-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-palladium.xml
 
 # Root
 PRODUCT_PACKAGES += \