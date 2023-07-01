ff81f32dc16a2d4054aa798b2ad007368fcb5854
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-01 16:17:26 +0000

    config: soong: Add support for miui camera workarounds
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: I3996868221474a1a076a7f77934305bf6af5ddb9
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,8 @@ SOONG_CONFIG_palladiumGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_uses_newer_hidl_override_format \
+    camera_needs_miui_camera_mode_support \
+    camera_needs_camera_needs_depth_sensor_override \
     camera_needs_client_info_lib_oplus \
     uses_nothing_camera \
     disable_bluetooth_le_read_buffer_size_v2 \
@@ -78,6 +80,8 @@ SOONG_CONFIG_palladiumGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GR
 SOONG_CONFIG_palladiumGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_palladiumGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_palladiumGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
+SOONG_CONFIG_palladiumGlobalVars_camera_needs_miui_camera_mode_support := $(TARGET_USES_MIUI_CAMERA)
+SOONG_CONFIG_palladiumGlobalVars_camera_needs_camera_needs_depth_sensor_override := $(TARGET_USES_DEPTHSENSOR_OVERRIDE)
 SOONG_CONFIG_palladiumGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
 SOONG_CONFIG_palladiumGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_palladiumGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
