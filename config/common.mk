w1zzyl/vendor_awaken

vendor: Add pixel_2016_exclusive.xml
Change-Id: I0fa851924368f1fa1bd1ac91ed86a7e9b8dd869e

config/common.mk
@@ -99,9 +99,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_land_l=64 \
     ro.com.google.ime.kb_pad_land_r=64
 
-# Google LILY_EXPERIENCE feature
+# Google features
 PRODUCT_COPY_FILES += \
-    vendor/awaken/prebuilt/common/etc/sysconfig/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
+    vendor/awaken/prebuilt/common/etc/sysconfig/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml \
+    vendor/awaken/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
 # Include AOSP audio files
 ifneq ($(USE_GAPPS),true)

Palladium-OS/platform_vendor_palladium

config/common.mk
@@ -99,9 +99,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_land_l=64 \
     ro.com.google.ime.kb_pad_land_r=64
 
-# Google LILY_EXPERIENCE feature
+# Google features
 PRODUCT_COPY_FILES += \
-    vendor/palladium/prebuilt/common/etc/sysconfig/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
+    vendor/palladium/prebuilt/common/etc/sysconfig/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml \
+    vendor/palladium/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
 # Include AOSP audio files
 ifneq ($(USE_GAPPS),true)